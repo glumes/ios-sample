409118db3fde2d0e017324b468a18a081da00a09
glumes <zhaoying9402@gmail.com>
2019-11-06 23:49:04 +0800

    add oc sample

---
+++ sample_oc/sample_oc/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  sample_oc
+//
+//  Created by glumes on 2019/11/6.
+//  Copyright © 2019 glumes. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
