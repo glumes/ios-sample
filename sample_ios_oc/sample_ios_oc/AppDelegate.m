b1d9fe64ca1674ee26219b7bef08a2b53234db95
glumes <zhaoying9402@gmail.com>
2019-11-07 23:42:26 +0800

    change

--- sample_ios_oc/sample_ios_oc/AppDelegate.m
+++ sample_ios_oc/sample_ios_oc/AppDelegate.m
@@ -17,6 +17,20 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    self.window = [[UIWindow alloc] initWithFrame: [[UIScreen mainScreen] bounds]];
+    
+    self.window.backgroundColor = [UIColor redColor];
+    
+    UIViewController *controller = [[UIViewController alloc] init];
+    self.window.rootViewController = controller;
+    
+    UIView * rootView = [[UIView alloc] initWithFrame: [[UIScreen mainScreen] bounds]];
+    
+    controller.view  = rootView;
+    
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
